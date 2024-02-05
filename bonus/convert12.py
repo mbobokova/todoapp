0aa23e9c9984d45da5b0f1da922452c8b88bc191
Macka <martina.bobokova@gmail.com>
2024-02-05 11:43:20 +0100

    add bonus 16 zip files

--- bonus/convert12.py
+++ bonus/convert12.py
@@ -1,3 +1,28 @@
+import PySimpleGUI as sg
+
+# First line
+feet_label = sg.Text('Enter feet: ')
+feet_input = sg.Input()
+
+
+# Second line
+inches_label = sg.Text('Enter inches: ')
+inches_input = sg.Input()
+
+
+convert_button = sg.Button("convert")
+
+
+window = sg.Window('Convertor',
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [convert_button]]
+                   )
+
+window.read()
+window.close()
+
+
 def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
